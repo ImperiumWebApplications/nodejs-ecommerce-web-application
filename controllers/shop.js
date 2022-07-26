db98ed9a1e4791bec90f1ab9f55f43de3070924d
Aditya <adityasingh220598@gmail.com>
2022-07-26 09:35:44 +0530

    Getting products from the database for the /products route

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,17 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.getProducts((products) => {
-    res.render("shop/product-list", {
-      products: products,
-      pageTitle: "All Products",
-      path: "/products",
+  Product.fetchAll()
+    .then(([products]) => {
+      res.render("shop/product-list", {
+        products: products,
+        pageTitle: "All Products",
+        path: "/products",
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  });
 };
 
 exports.getIndex = (req, res, next) => {
