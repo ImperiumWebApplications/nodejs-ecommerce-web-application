e0aec4ec59aaca9d9752652b0eeae4d1c609aa86
Aditya <adityasingh220598@gmail.com>
2022-07-27 09:34:47 +0530

    Adding relations between product and user models

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 
 const db = require("./util/database");
 const errorController = require("./controllers/error");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -21,6 +23,10 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+// A product belongs to a user
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
 
 // Sync the database using sequelize
 db.sync()
