766274ca2b3b6f3a0305667ab8be13a703237ef1
Aditya <adityasingh220598@gmail.com>
2022-08-03 09:43:50 +0530

    Introducing mongoose

--- app.js
+++ app.js
@@ -2,41 +2,46 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database");
-const User = require("./models/user");
+// const User = require("./models/userMongoDB");
 
 const app = express();
 
 // Configure ejs as the template engine
 app.set("view engine", "ejs");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Find the user in the database and set it as the value for req.user
-app.use((req, res, next) => {
-  User.findById("62e5f435347621d42f3000e0")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+// app.use((req, res, next) => {
+//   User.findById("62e5f435347621d42f3000e0")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect()
+mongoose
+  .connect(
+    "mongodb+srv://root:tiktik123@cluster0.lhsfo.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
   .then(() => {
+    console.log("Connected to database");
     app.listen(3000);
   })
   .catch((err) => {
---
+++ models/productmongoDB.js
@@ -0,0 +1,96 @@
+const getDB = require("../util/database");
+const mongoDB = require("mongodb");
+
+class Product {
+  constructor(name, price, imageUrl, description, userId) {
+    this.name = name;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.userId = userId;
+  }
+
+  save() {
+    return getDB().then((db) => {
+      return db
+        .collection("products")
+        .insertOne(this)
+        .then((result) => {
+          this._id = result.insertedId;
+          return this;
+        });
+    });
+  }
+
+  static updateById(id, name, price, imageUrl, description) {
+    return getDB().then((db) => {
+      return db
+        .collection("products")
+        .updateOne(
+          { _id: mongoDB.ObjectId(id) },
+          {
+            $set: {
+              name: name,
+              price: price,
+              imageUrl: imageUrl,
+              description: description,
+            },
+          }
+        )
+        .then((result) => {
+          return result;
+        });
+    });
+  }
+
+  static fetchAll() {
+    return getDB().then((db) => {
+      return db
+        .collection("products")
+        .find()
+        .toArray()
+        .then((products) => {
+          return products.map((product) => {
+            return {
+              id: product._id,
+              name: product.name,
+              price: product.price,
+              imageUrl: product.imageUrl,
+              description: product.description,
+            };
+          });
+        });
+    });
+  }
+
+  static findById(id) {
+    return getDB().then((db) => {
+      return db
+        .collection("products")
+        .find({ _id: new mongoDB.ObjectId(id) })
+        .next()
+        .then((product) => {
+          return {
+            id: product._id,
+            name: product.name,
+            price: product.price,
+            imageUrl: product.imageUrl,
+            description: product.description,
+          };
+        });
+    });
+  }
+
+  static deleteById(id) {
+    return getDB().then((db) => {
+      return db
+        .collection("products")
+        .deleteOne({ _id: new mongoDB.ObjectId(id) })
+        .then((result) => {
+          return result;
+        });
+    });
+  }
+}
+
+module.exports = Product;
