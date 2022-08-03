57562336f28f9809f97c25c185f0b578773e9025
Aditya <adityasingh220598@gmail.com>
2022-08-03 09:48:34 +0530

    Creating the product schema

--- models/product.js
+++ models/product.js
@@ -1,10 +1,27 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  title: String,
-  price: Number,
-  description: String,
-  image: String,
+  title: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
 });
 
 const Product = mongoose.model("Product", productSchema);
